a24d082195fb2bbb636be26db80c78f067488f3b
calossandres <163074147+calossandres@users.noreply.github.com>
2024-05-16 03:13:20 -0500

    arreglo indexPage quitar import no usado

--- app/indexPage/page.js
+++ app/indexPage/page.js
@@ -1,5 +1,5 @@
 // Importa los componentes necesarios de Clerk
-import { RedirectToHome, SignedIn } from "@clerk/nextjs";
+import { SignedIn } from "@clerk/nextjs";
 
 // Componente IndexPage
 export default function IndexPage() {
@@ -8,7 +8,9 @@ export default function IndexPage() {
       {/* Componente SignedIn para mostrar contenido solo a usuarios autenticados */}
       <SignedIn>
         {/* Contenido solo visible para usuarios autenticados */}
+
         <div>Bienvenido a la página de inicio</div>
+        
       </SignedIn>
       {/* Contenido adicional de la página */}
       <div>inicie sesión para ver el contenido de la pagina ...</div>
