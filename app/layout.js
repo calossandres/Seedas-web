865c3209d6ab17983debf33b46893279e5e9fd72
calossandres <163074147+calossandres@users.noreply.github.com>
2024-05-25 19:37:35 -0500

    <SignedIn> -> para solo permitir vista indexPage a usarios logueados

--- app/layout.js
+++ app/layout.js
@@ -8,7 +8,7 @@ import { ClerkProvider, SignedIn, SignedOut } from '@clerk/nextjs'
 // Importación del componente de encabezado
 import Header from '@/app/components/Header'
 
-
+import IndexPage from "./indexPage/page";
 // Definición de las fuentes a utilizar
 const inter = Montserrat({ subsets: ["latin"] });
 
@@ -32,7 +32,9 @@ export default function RootLayout({ children }) {
           <Header />
           
           {/* Contenido principal de la página */}
-      
+          <SignedIn>
+          <IndexPage/>
+          </SignedIn>
           {children}
          
         </body>
