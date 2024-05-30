018cb5baebae1f2c96261f30c97cc06801cc233a
calossandres <163074147+calossandres@users.noreply.github.com>
2024-05-30 02:27:26 -0500

    autocomplete en inputItem.js

--- app/components/Home/InputItem.js
+++ app/components/Home/InputItem.js
@@ -1,17 +1,38 @@
-import Image from 'next/image'
-import React from 'react'
+"use client";
+import React, { useState } from 'react';
+import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
 
-function InputItem({type}) {
+const InputItem = ({ type }) => {
+  const [value, setValue] = useState(null);
+
   return (
-    <div className='bg-slate-200 p-3 rounded-lg mt-3 
-    flex items-center gap-4'>
-        <Image src={type=='source'?'/source.png':'/source.png'}width={15} height={15}/>
-        <input type='text'
-        placeholder={type=='source'?'Pickup Location':'Dropoff location' }
-        className='bg-trasparent w-full outline-none '/>
-
+    <div>
+      <input
+        type="text"
+        className="bg-transparent w-full outline-none"
+      />
+      <GooglePlacesAutocomplete
+        apiKey={process.env.NEXT_PUBLIC_GOOGLE_API_KEY}
+        selectProps={{
+          value,
+          onChange: setValue,
+          placeholder: 'pickup Location',
+          isClearable: true,
+          className: 'w-full',
+          components: {
+            DropdownIndicator: false,
+          },
+          styles: {
+            control: (provided) => ({
+              ...provided,
+              backgroundColor: '#00ffff',
+              border: 'none',
+            }),
+          },
+        }}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default InputItem
+export default InputItem;
